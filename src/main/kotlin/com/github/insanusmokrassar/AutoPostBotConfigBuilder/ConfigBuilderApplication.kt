69c33235fe117fb39b05958894e361a1660c974c
000Sanya <000sanya.000sanya@gmail.com>
2018-07-18 19:16:37 +1000

    implement very base functional

--- src/main/kotlin/com/github/insanusmokrassar/AutoPostBotConfigBuilder/ConfigBuilderApplication.kt
+++ src/main/kotlin/com/github/insanusmokrassar/AutoPostBotConfigBuilder/ConfigBuilderApplication.kt
@@ -2,14 +2,17 @@ package com.github.insanusmokrassar.AutoPostBotConfigBuilder
 
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
 
 class ConfigBuilderApplication : Application() {
     override fun start(stage: Stage) {
-        stage.scene = Scene(
-            FXMLLoader.load(this::class.java.classLoader.getResource("ConfigBuilder.fxml"))
-        )
+        val loader = FXMLLoader(this::class.java.classLoader.getResource("ConfigBuilder.fxml"))
+        val parent = loader.load() as? Parent
+        val controller = loader.getController() as? GlobalConfigController
+        controller?.stage = stage;
+        stage.scene = Scene(parent)
         stage.show()
     }
 
--- src/main/kotlin/com/github/insanusmokrassar/AutoPostBotConfigBuilder/GlobalConfigController.kt
+++ src/main/kotlin/com/github/insanusmokrassar/AutoPostBotConfigBuilder/GlobalConfigController.kt
@@ -1,12 +1,21 @@
 package com.github.insanusmokrassar.AutoPostBotConfigBuilder
 
+import com.github.insanusmokrassar.AutoPostBotConfigBuilder.models.Config
+import com.github.insanusmokrassar.AutoPostBotConfigBuilder.models.DatabaseConfig
+import com.github.insanusmokrassar.AutoPostBotConfigBuilder.models.ProxySettings
+import com.github.insanusmokrassar.IObjectKRealisations.toJson
+import com.github.insanusmokrassar.IObjectKRealisations.toObject
 import javafx.event.EventHandler
 import javafx.fxml.FXML
+import javafx.fxml.Initializable
 import javafx.scene.control.*
+import javafx.stage.FileChooser
+import javafx.stage.Stage
 import java.net.URL
 import java.util.*
 
-class GlobalConfigController {
+class GlobalConfigController : Initializable {
+
     @FXML private lateinit var openButton: MenuItem
     @FXML private lateinit var saveButton: MenuItem
 
@@ -28,11 +37,68 @@ class GlobalConfigController {
     @FXML private lateinit var proxyUsername: TextField
     @FXML private lateinit var proxyPassword: TextField
 
-    @FXML
-    private fun initialize() {
+    lateinit var stage: Stage
+
+    private lateinit var config: Config
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
         openButton.onAction = EventHandler {
-            println(it)
+            val filechooser = FileChooser()
+            filechooser.title = "Open configuration file"
+            val file = filechooser.showOpenDialog(stage)
+            val content = file?.readText()
+            config = if(content != null) {
+                content.toObject(Config::class.java)
+            } else throw Exception()
+            updateView()
+        }
+
+        saveButton.onAction = EventHandler {
+            viewToConfig()
+            val fileChooser = FileChooser()
+            fileChooser.title = "Save configuration file"
+            val file = fileChooser.showSaveDialog(stage)
+            file.writeText(config.toJson())
         }
         println("Initialized")
+    }
+
+    private fun updateView() {
+        sourceChatId.text = config.sourceChatId.toString()
+        targetChatId.text = config.targetChatId.toString()
+        logsChatId.text = config.logsChatId.toString()
+        botToken.text = config.botToken
+
+        dbUrl.text = config.databaseConfig?.url
+        dbDriver.text = config.databaseConfig?.driver
+        dbUsername.text = config.databaseConfig?.username
+        dbPassword.text = config.databaseConfig?.password
+
+        proxyHost.text = config.proxy?.host
+        proxyPort.text = config.proxy?.port.toString()
+        proxyUsername.text = config.proxy?.username
+        proxyPassword.text=  config.proxy?.password
+    }
+
+    private fun viewToConfig() {
+        config = Config(
+                sourceChatId.text.toLong(),
+                targetChatId.text.toLong(),
+                botToken.text,
+                logsChatId.text.toLong(),
+                databaseConfig = DatabaseConfig(
+                        dbUrl.text,
+                        dbDriver.text,
+                        dbUsername.text,
+                        dbPassword.text
+                ),
+                proxy = ProxySettings(
+                        proxyHost.text,
+                        proxyPort.text.toInt(),
+                        proxyUsername.text,
+                        proxyPassword.text
+                )
+
+        )
     }
 }
